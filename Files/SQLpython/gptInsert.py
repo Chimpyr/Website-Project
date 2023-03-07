c7e0a6f471083ba566abeedf6f953e09e675b30a
Jacob <jacob.craig03@gmail.com>
2023-03-07 15:14:04 +0000

    ready for database manipulation

--- Files/SQLpython/gptInsert.py
+++ Files/SQLpython/gptInsert.py
@@ -11,6 +11,7 @@ mydb = mysql.connector.connect(
     password="pass",
     database='horizon_travels_test'
 )
+
 cursor = mydb.cursor()
 
 # Get column names for table
@@ -23,13 +24,23 @@ for column in columns:
     value = input(f"Enter value for {column}: ")
     values.append(value)
 
-# Construct SQL query to insert data into table
-query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s' for _ in columns])})"
-cursor.execute(query, values)
-mydb.commit()
+# Check for duplicates
+query = f"SELECT * FROM {table_name} WHERE "
+for i, column in enumerate(columns):
+    query += f"{column} = '{values[i]}'"
+    if i < len(columns) - 1:
+        query += " AND "
+cursor.execute(query)
+result = cursor.fetchone()
+if result:
+    print("Data already exists in the table!")
+else:
+    # Construct SQL query to insert data into table
+    query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s' for _ in columns])})"
+    cursor.execute(query, values)
+    mydb.commit()
+    print("Data inserted successfully!")
 
 # Close database connection
 cursor.close()
 mydb.close()
-
-print("Data inserted successfully!")
