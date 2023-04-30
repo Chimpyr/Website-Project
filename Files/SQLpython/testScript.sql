87bb1b21bc88978b466defc0c84c3041fa7a7d4c
Jacob <jacob.craig03@gmail.com>
2023-04-30 22:59:34 +0100

    Fixed edit booking, adjusting for DB change

--- Files/SQLpython/testScript.sql
+++ Files/SQLpython/testScript.sql
@@ -10,6 +10,9 @@ select * from journey;
 delete from users;
 delete from booking;
 
+DELETE FROM booking
+WHERE start_location LIKE '%Glasgow%';
+
 update users SET usertype = 'admin' WHERE email = "admin@admin.com";
 
 INSERT INTO users (first_name, last_name, password_hash, email, usertype) VALUES ('J','C', 'securePassword','jc@jc.com', 'admin');
